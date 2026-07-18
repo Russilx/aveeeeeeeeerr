fc2b77bcefe16369f3bad84f554d0752f69edc42
Russilx <sanmartinesteban123@gmail.com>
2026-07-18 01:13:07 -0300

    Create music-config.js with initial track list
    Added a music configuration file with a sample track.

---
+++ music-config.js
@@ -0,0 +1,16 @@
+// Lista de canciones de fondo para la pantalla de login.
+// Poné acá los nombres de archivo (subidos a una carpeta "music/"
+// en el mismo repo). Se van a ir reproduciendo en orden y, al
+// terminar la última, vuelve a empezar de la primera.
+//
+// Ejemplo:
+// const MUSIC_TRACKS = [
+//   'music/tema1.mp3',
+//   'music/tema2.mp3',
+//   'music/tema3.mp3'
+// ];
+ 
+const MUSIC_TRACKS = [
+  'music/tema1.mp3'
+];
+ 
