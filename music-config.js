f5ff1d678c1fc45d483e2f3b887b9d0b68c00f0d
Russilx <sanmartinesteban123@gmail.com>
2026-07-18 01:25:35 -0300

    Update music-config.js

--- music-config.js
+++ music-config.js
@@ -1,4 +1,4 @@
 // Lista de canciones de fondo para la pantalla de login.
 // Poné acá los nombres de archivo. Se van a ir reproduciendo en
 // orden y, al terminar la última, vuelve a empezar de la primera.
-const MUSIC_TRACKS = ["musica1.mp3", "musica2.mp3"];
+const MUSIC_TRACKS = ["musica2.mp3", "musica1.mp3"];
